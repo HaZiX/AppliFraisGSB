f9d57258dc9dfcb40b2676c3db54afd2a490368d
Antonin Catrix <bpis@hotmail.fr>
2018-12-11 09:30:28 +0100

    gitignore

---
+++ AppliFraisGSB/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Data;
+using System.Windows.Input;
+using System.Windows.Media;
+using MySql.Data.MySqlClient;
+using System.Data.SqlClient;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AppliFraisGSB
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Personne secretaire;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            string connectionString = "SERVER=localhost;DATABASE=appli_frais;UID=root;PASSWORD=;";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            MySqlCommand cmd = new MySqlCommand("SELECT * FROM secretaire;", connection);
+            connection.Open();
+            MySqlDataReader read = cmd.ExecuteReader();
+            read.Read();
+            secretaire = new Personne(read["prenom"].ToString(), read["nom"].ToString(), read["mail"].ToString(), read["telephone"].ToString(), read["mdp"].ToString(), read["sexe"].ToString());
+            connection.Close();
+        }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (textName.Text == secretaire.mail && textPassword.Password == secretaire.password)
+            {
+                HomeWindow main = new HomeWindow();
+                main.Role = "secretaire";
+                App.Current.MainWindow = main;
+                this.Close();
+                main.Show();
+            }
+        }
+    }
+}
---
+++ AppliFraisGSB/Personne.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppliFraisGSB
+{
+    class Personne
+    {
+        public string name { get; }
+        public string surname { get; }
+        public string mail { get; }
+        public string phone { get; }
+        public string password { get; }
+        public string sexe { get; }
+
+        public Personne(string name, string surname, string mail, string phone, string password, string sexe)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.mail = mail;
+            this.phone = phone;
+            this.password = password;
+            this.sexe = sexe;
+        }
+    }
+}
